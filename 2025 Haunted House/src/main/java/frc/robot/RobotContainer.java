e7a14104c196421f2b1b58e0b8a3546c7c299a61
CrolineCrois <maoninii@gmail.com>
2024-10-07 14:11:40 -0700

    remove example susystem

--- 2025 Haunted House/src/main/java/frc/robot/RobotContainer.java
+++ 2025 Haunted House/src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,6 @@
 package frc.robot;
 
 import frc.robot.Constants.OperatorConstants;
-import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
@@ -18,7 +17,6 @@ import edu.wpi.first.wpilibj2.command.button.Trigger;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-  private final ExampleSubsystem m_exampleSubsystem = new ExampleSubsystem();
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
